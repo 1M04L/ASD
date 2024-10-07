f2fd1f76d0739921e95b04122d278be67bb97827
1M04L <mikhailulianenko@gmail.com>
2024-10-07 16:50:10 +0700

    Update BM.cpp
    hand-input added

--- 2 lab/BM.cpp
+++ 2 lab/BM.cpp
@@ -1,42 +1,48 @@
-#include <iostream>
-#include <string>
-#include <vector>
 
-int boyerMooreSearch(const std::string& text, const std::string& pattern) {
-	std::vector<int> shiftTable(256, pattern.size());
-	for (int i = 0; i < pattern.size() - 1; ++i) {
-		shiftTable[pattern[i]] = pattern.size() - i - 1;
-	}
+#include <iostream> 
+#include <string> 
+#include <vector> 
+ 
+int boyerMooreSearch(const std::string& text, const std::string& pattern) { 
+ std::vector<int> shiftTable(256, pattern.size()); 
+ for (int i = 0; i < pattern.size() - 1; ++i) { 
+  shiftTable[pattern[i]] = pattern.size() - i - 1; 
+ } 
+ 
+ int textIndex = pattern.size() - 1; 
+ int patternIndex = pattern.size() - 1; 
+ 
+ while (textIndex < text.size()) { 
+  if (text[textIndex] == pattern[patternIndex]) { 
+   if (patternIndex == 0) { 
+    return textIndex; 
+   } 
+   --textIndex; 
+   --patternIndex; 
+  } else { 
+   textIndex += shiftTable[text[textIndex]]; 
+   patternIndex = pattern.size() - 1; 
+  } 
+ } 
+ 
+ return -1; 
+} 
+ 
+int main() { 
+    std::cout<<"use only englisj symbols"<<std::endl;
+    std::string text;
+    std::string pattern;
+    std::cout << "string?:   ";
+    std::getline(std::cin, text); 
 
-	int textIndex = pattern.size() - 1;
-	int patternIndex = pattern.size() - 1;
-
-	while (textIndex < text.size()) {
-		if (text[textIndex] == pattern[patternIndex]) {
-			if (patternIndex == 0) {
-				return textIndex;
-			}
-			--textIndex;
-			--patternIndex;
-		} else {
-			textIndex += shiftTable[text[textIndex]];
-			patternIndex = pattern.size() - 1;
-		}
-	}
-
-	return -1;
-}
-
-int main() {
-	std::string text = "ABFFFFFFFCABCAB";
-	std::string pattern = "CAB";
-	int index = boyerMooreSearch(text, pattern);
-
-	if (index != -1) {
-		std::cout << "input position of yr template: " << index << std::endl;
-	} else {
-		std::cerr << "cant find yr template" << std::endl;
-	}
-	return 0;
-}
+    std::cout << "substring?(aka template aka pattern): ";
+    std::getline(std::cin, pattern);
+ int index = boyerMooreSearch(text, pattern); 
+ 
+ if (index != -1) { 
+  std::cout << "input position of yr template: " << index << std::endl; 
+ } else { 
+  std::cerr << "cant find yr template" << std::endl; 
+ } 
+ return 0; 
 
