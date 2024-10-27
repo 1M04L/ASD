02ea5be74213c46f35981daad8189b3eea3db651
1M04L <mikhailulianenko@gmail.com>
2024-10-27 13:38:51 +0700

    Update BM.cpp

--- 2 lab/BM.cpp
+++ 2 lab/BM.cpp
@@ -1,48 +1,104 @@
 
-#include <iostream> 
-#include <string> 
-#include <vector> 
- 
-int boyerMooreSearch(const std::string& text, const std::string& pattern) { 
- std::vector<int> shiftTable(256, pattern.size()); 
- for (int i = 0; i < pattern.size() - 1; ++i) { 
-  shiftTable[pattern[i]] = pattern.size() - i - 1; 
- } 
- 
- int textIndex = pattern.size() - 1; 
- int patternIndex = pattern.size() - 1; 
- 
- while (textIndex < text.size()) { 
-  if (text[textIndex] == pattern[patternIndex]) { 
-   if (patternIndex == 0) { 
-    return textIndex; 
-   } 
-   --textIndex; 
-   --patternIndex; 
-  } else { 
-   textIndex += shiftTable[text[textIndex]]; 
-   patternIndex = pattern.size() - 1; 
-  } 
- } 
- 
- return -1; 
-} 
- 
-int main() { 
-    std::cout<<"use only englisj symbols"<<std::endl;
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<int> createShiftTable(const std::string& pattern) {
+    const int maxSizeChar = 256;
+    std::vector<int> shiftTable(maxSizeChar, pattern.size());
+    int m = pattern.size();
+    for (int i = 0; i < m - 1; ++i) {
+        shiftTable[pattern[i]] = m - i - 1;
+    }
+    return shiftTable;
+}
+
+void searchPattern(const std::string& text, const std::string& pattern, int start, int end, std::vector<int>& occurrence, bool findFirstOnly) {
+    int sizeText = text.size();
+    int sizePattern = pattern.size();
+
+    if (sizePattern == 0 || sizeText < sizePattern || start < 0 || end >= sizeText || start > end) {
+        return;
+    }
+
+    std::vector<int> shiftTable = createShiftTable(pattern);
+
+    int shift = start;
+    while (shift <= end - sizePattern + 1) {
+        int j = sizePattern - 1;
+
+        while (j >= 0 && pattern[j] == text[shift + j]) {
+            --j;
+        }
+
+        if (j < 0) {
+            occurrence.push_back(shift);
+            if (findFirstOnly) {
+                return;
+            }
+            if (shift + sizePattern <= end) {
+                shift += sizePattern;
+            } else {
+                shift += 1;
+            }
+        } else {
+            int badChar = text[shift + j];
+            shift += shiftTable[badChar];
+        }
+    }
+}
+
+int findFirstPattern(const std::string& text, const std::string& pattern) {
+    std::vector<int> occurrence;
+    searchPattern(text, pattern, 0, text.size() - 1, occurrence, true);
+    if (occurrence.empty()){
+        return -1;
+      } else {
+        return occurrence[0];
+      }
+}
+
+std::vector<int> findAllOccurrence(const std::string& text, const std::string& pattern) {
+    std::vector<int> occurrence;
+    searchPattern(text, pattern, 0, text.size() - 1, occurrence, false);
+    return occurrence;
+}
+
+std::vector<int> findAllIndexOccurrence(const std::string& text, const std::string& pattern, int start, int end) {
+    std::vector<int> occurrence;
+    searchPattern(text, pattern, start, end, occurrence, false);
+    return occurrence;
+}
+
+int main() {
     std::string text;
     std::string pattern;
-    std::cout << "string?:   ";
-    std::getline(std::cin, text); 
 
-    std::cout << "substring?(aka template aka pattern): ";
+    std::cout << "Введите строку: ";
+    std::getline(std::cin, text);
+
+    std::cout << "Введите подстроку: ";
     std::getline(std::cin, pattern);
- int index = boyerMooreSearch(text, pattern); 
- 
- if (index != -1) { 
-  std::cout << "input position of yr template: " << index << std::endl; 
- } else { 
-  std::cerr << "cant find yr template" << std::endl; 
- } 
- return 0; 
 
+    int firstOccurrence = findFirstPattern(text, pattern);
+    std::cout << "First occurrence: " << firstOccurrence << std::endl;
+
+    std::vector<int> allOccurrences = findAllOccurrence(text, pattern);
+    std::cout << "All occurrences: ";
+    for (int i = 0; i < allOccurrences.size(); ++i) {
+        std::cout << allOccurrences[i] << " ";
+    }
+    std::cout << std::endl;
+
+    int start = 0;
+    int end = text.size() - 1;
+    std::vector<int> rangeOccurrences = findAllIndexOccurrence(text, pattern, start, end);
+
+    std::cout << "Occurrences in the range: ";
+    for (int i = 0; i < rangeOccurrences.size(); ++i) {
+        std::cout << rangeOccurrences[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
