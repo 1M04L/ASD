ab7c14cd9aae5b7ac83e4c69f4e3d8ff57e6ec38
1M04L <mikhailulianenko@gmail.com>
2024-11-28 12:33:42 +0700

    generation of array as a fucntion added

--- 5th lab(Hoare).cpp
+++ 5th lab(Hoare).cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <vector>
 
-void isSorted(int arr[], int n) {
-    for (int i = 1; i < n; i++) {
+std::vector<int> generateArray(int n) {
+    srand(time(0));
+    std::vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 100;
+    }
+    return arr;
+}
+
+
+void isSorted(const std::vector<int>& arr) {
+    for (size_t i = 1; i < arr.size(); i++) {
         if (arr[i - 1] > arr[i]) {
             std::cout << "array was not sorted" << std::endl;
             return;
         }
     }
-    std::cout << "array was sorted" << std::endl;
+    std::cout << "  array was sorted" << std::endl;
 }
 
-void quickSort(int arr[], int low, int high) {
+
+void quickSort(std::vector<int>& arr, int low, int high) {
     if (low < high) {
         int pivot = arr[high];
-        int i = (low - 1); 
+        int i = (low - 1);
 
         for (int j = low; j <= high - 1; j++) {
             if (arr[j] < pivot) {
@@ -25,7 +37,7 @@ void quickSort(int arr[], int low, int high) {
         }
         std::swap(arr[i + 1], arr[high]);
 
-        int partitionIndex = i + 1; 
+        int partitionIndex = i + 1;
 
         quickSort(arr, low, partitionIndex - 1);
         quickSort(arr, partitionIndex + 1, high);
@@ -33,27 +45,22 @@ void quickSort(int arr[], int low, int high) {
 }
 
 int main() {
-    srand(time(0)); 
-
-    const int n = 10; 
-    int arr[n];
+    const int n = 11;
+    std::vector<int> arr = generateArray(n);
 
     std::cout << "given array: ";
-    for (int i = 0; i < n; i++) {
-        arr[i] = rand() % 100;
-        std::cout << arr[i] << " ";
-    }
+    for (int x : arr) std::cout << x << " ";
     std::cout << std::endl;
 
     quickSort(arr, 0, n - 1);
 
     std::cout << " sorted array: ";
-    for (int i = 0; i < n; i++) {
-        std::cout << arr[i] << " ";
+    for (int x : arr) {
+        std::cout << x << " ";
     }
     std::cout << std::endl;
 
-    isSorted(arr, n);
+    isSorted(arr);
 
     return 0;
 }
