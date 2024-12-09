33db8e76a2093ca810acb47f939cd0b6410a5d01
1M04L <mikhailulianenko@gmail.com>
2024-12-09 11:25:33 +0700

    Update (6th lab)bitwiseCountingSort.cpp

--- (6th lab)bitwiseCountingSort.cpp
+++ (6th lab)bitwiseCountingSort.cpp
@@ -11,46 +11,41 @@ bool isSorted(const std::vector<int>& arr) {
     return true;
 }
 
-void bitwiseCountingSort(std::vector<int>& arr) {
-    if (arr.empty()) return;
+void bitwiseSort(std::vector<int>& arr, int l, int r, int k) {
+    if (l >= r || k < 0) return;
 
-    int maxVal = *std::max_element(arr.begin(), arr.end());
-    int numBits = 0;
-    int temp = maxVal;
-    while (temp > 0) {
-        numBits++;
-        temp >>= 1;
+    int i = l, j = r;
+    while (i <= j) {
+        while (i <= j && !((arr[i] >> k) & 1)) i++;
+        while (i <= j && ((arr[j] >> k) & 1)) j--;
+
+        if (i < j) {
+            std::swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
     }
 
-
-    std::vector<int> output(arr.size());
-    std::vector<int> count(1 << numBits, 0);
-
-
-    for (int x : arr) {
-        count[x]++;
-    }
-
-    for (int i = 1; i < count.size(); i++) {
-        count[i] += count[i - 1];
-    }
-
-    for (int i = arr.size() - 1; i >= 0; i--) {
-        output[count[arr[i]] - 1] = arr[i];
-        count[arr[i]]--;
-    }
-
-    arr = output;
+    bitwiseSort(arr, l, j, k - 1);
+    bitwiseSort(arr, i, r, k - 1);
 }
 
 
 int main() {
-    std::vector<int> arr = {10, 5, 2, 8, 1, 9, 3, 7, 4, 6};
+    std::vector<int> arr = {1488,0, 5, 2, 228, 1, 9, 3, 7, 4, 6};
+    int maxVal = *std::max_element(arr.begin(), arr.end());
+    int maxBit = 0;
+    int temp = maxVal;
+    while (temp > 0) {
+      maxBit++;
+      temp >>= 1;
+    }
+
     std::cout << "original array: ";
     for (int x : arr) std::cout << x << " ";
     std::cout << std::endl;
 
-    bitwiseCountingSort(arr);
+    bitwiseSort(arr, 0, arr.size() - 1, maxBit -1);
 
     std::cout << "sorted array: ";
     for (int x : arr) std::cout << x << " ";
@@ -59,7 +54,7 @@ int main() {
     if (isSorted(arr)) {
         std::cout << "array has been sorted" << std::endl;
     } else {
-        std::cerr<< "array wasnt sorted" << std::endl;
+        std::cerr << "array wasnt sorted" << std::endl;
     }
 
     return 0;
